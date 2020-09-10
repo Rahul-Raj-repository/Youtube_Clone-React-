963e267f92b31ec627ea17b6f896e17a6e996bb0
Rahul Raj <rraj5468@gmail.com>
2020-09-10 17:06:55 +0530

    Updates

---
+++ precache-manifest.584911e44fb61acaa6f03c58aaa93e97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087133f91c3a34a20c5ceb6783a48151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d6130abfdbd44da04d8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ccbd30cde452c43c6f9",
+    "url": "/static/js/2.a64da3f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a64da3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6130abfdbd44da04d8",
+    "url": "/static/js/main.42f9fb5d.chunk.js"
+  },
+  {
+    "revision": "128f8c2e326c2d21e842",
+    "url": "/static/js/runtime-main.fb489aeb.js"
+  }
+]);
